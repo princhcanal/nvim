a44c0c6027322482c589090db28a2d6fb567751a
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-18 15:39:30 +0800

    add gitsigns

--- lua/mvprinch/packer.lua
+++ lua/mvprinch/packer.lua
@@ -56,4 +56,8 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+  use {
+    'lewis6991/gitsigns.nvim',
+    -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  }
 end)
