e376533f4718cf9fbc2ee006e5e8462cd57aceb1
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-18 16:01:02 +0800

    add format on save

--- lua/mvprinch/init.lua
+++ lua/mvprinch/init.lua
@@ -8,27 +8,28 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+  require("plenary.reload").reload_module(name)
 end
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
-autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
-    pattern = "*",
-    command = "%s/\\s\\+$//e",
+autocmd({ "BufWritePre" }, {
+  group = ThePrimeagenGroup,
+  pattern = "*",
+  command = "%s/\\s\\+$//e",
 })
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting()]]
