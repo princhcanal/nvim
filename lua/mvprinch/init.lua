71473f391ea43f7662dd89ff66d9c16acedf7bc5
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-18 19:53:45 +0800

    format sync

--- lua/mvprinch/init.lua
+++ lua/mvprinch/init.lua
@@ -32,4 +32,4 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
