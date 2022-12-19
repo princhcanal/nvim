750f6184648bc9ca7d70a8ad1f8209205691e94e
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-19 21:33:10 +0800

    remove comment remaps
    Please enter the commit message for your changes. Lines starting

--- lua/mvprinch/remap.lua
+++ lua/mvprinch/remap.lua
@@ -48,9 +48,6 @@ vim.keymap.set("n", "<leader>l", "<cmd>wincmd l<CR>")
 vim.keymap.set("n", "<leader>j", "<cmd>wincmd j<CR>")
 vim.keymap.set("n", "<leader>k", "<cmd>wincmd k<CR>")
 
-vim.keymap.set("n", "<leader>/", "<cmd>CommentToggle<CR>")
-vim.keymap.set("v", "<leader>/", "<cmd>'<,'>CommentToggle<CR>")
-
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
