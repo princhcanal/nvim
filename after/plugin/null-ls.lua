5d5fd944e1092a5b5179262d2b734c97716ab82f
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-21 09:51:14 +0800

    add null-ls

---
+++ after/plugin/null-ls.lua
@@ -0,0 +1,70 @@
+local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.prismaFmt,
+		null_ls.builtins.formatting.pg_format,
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.sqlfluff.with({
+			extra_args = { "--dialect", "postgres" },
+		}),
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.code_actions.eslint_d,
+	},
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
+})
+
+local no_really = {
+	method = null_ls.methods.DIAGNOSTICS,
+	filetypes = { "markdown", "text" },
+	generator = {
+		fn = function(params)
+			local diagnostics = {}
+			-- sources have access to a params object
+			-- containing info about the current file and editor state
+			for i, line in ipairs(params.content) do
+				local col, end_col = line:find("really")
+				if col and end_col then
+					-- null-ls fills in undefined positions
+					-- and converts source diagnostics into the required format
+					table.insert(diagnostics, {
+						row = i,
+						col = col,
+						end_col = end_col + 1,
+						source = "no-really",
+						message = "Don't use 'really!'",
+						severity = vim.diagnostic.severity.WARN,
+					})
+				end
+			end
+			return diagnostics
+		end,
+	},
+}
+
+null_ls.register(no_really)
--- lua/mvprinch/init.lua
+++ lua/mvprinch/init.lua
@@ -2,34 +2,34 @@ require("mvprinch.remap")
 require("mvprinch.set")
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local ThePrimeagenGroup = augroup("ThePrimeagen", {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-  require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
-autocmd('TextYankPost', {
-  group = yank_group,
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = 'IncSearch',
-      timeout = 40,
-    })
-  end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 autocmd({ "BufWritePre" }, {
-  group = ThePrimeagenGroup,
-  pattern = "*",
-  command = "%s/\\s\\+$//e",
+	group = ThePrimeagenGroup,
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
 })
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+-- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
