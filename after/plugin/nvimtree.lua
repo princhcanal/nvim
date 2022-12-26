43a306f5155792fb7c4835c29facf812cbff20b9
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-26 09:53:44 +0800

    add hidden files to search and nvim-tree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -9,21 +9,26 @@ vim.opt.termguicolors = true
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 500,
+	},
 })
 
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>")
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,17 +1,25 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>F', builtin.live_grep, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
 
-require('telescope').setup({
-  defaults = {
-    layout_strategy = 'vertical',
-    layout_config = {
-      height = 0.95,
-      width = 0.95
-      -- other layout configuration here
-    },
-    -- other defaults configuration here
-  },
-  -- other configuration values here
+require("telescope").setup({
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+		git_files = {
+			hidden = true,
+		},
+	},
+	defaults = {
+		layout_strategy = "vertical",
+		layout_config = {
+			height = 0.95,
+			width = 0.95,
+			-- other layout configuration here
+		},
+		-- other defaults configuration here
+	},
+	-- other configuration values here
 })
