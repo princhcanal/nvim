33a042d79a93187e9921d3f58e0e435275a81d7d
Princh Harold Cañal <princhcanal@gmail.com>
2023-06-27 18:22:55 +0800

    feat: add remap for listing buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 vim.keymap.set("n", "<leader>F", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>bu", builtin.buffers, {})
 
 require("telescope").setup({
 	pickers = {
